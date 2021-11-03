4f342ce48d1117929336afbf187d32ad5317e1d9
Avery Pace <blainepace@gmail.com>
2021-11-02 20:39:33 -0400

    Initial Commit

---
+++ Toki Trainer/Toki_TrainerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Toki_TrainerApp.swift
+//  Toki Trainer
+//
+//  Created by Avery Ada Pace on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Toki_TrainerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
